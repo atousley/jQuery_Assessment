e6bdd19c0a4664aea6c7199e7f3cfe9082c8121d
atousley <annette.tousley@gmail.com>
2016-02-08 11:58:55 -0600

    updated with full functionality

--- app.js
+++ app.js
@@ -3,20 +3,29 @@ var counter = 0;
 $(document).ready(function() {
 
 	$('#generate').append('<button class="genButton">Generate</button>');
-	$('#generate').append('<p class="numClicks"></p>');
+	$('#generate').append('<p class="numClicks">Click Number: 0</p>');
 	$('.genButton').on('click', domButtonAdd);
+	$('.container').on('click', '.delete', deleteButton);
+	$('.container').on('click', '.change', changeColor);
 });
 
-function domButtonAdd () {
+function domButtonAdd() {
 	counter++;
 
-	$('#generate').append('<div class="container"></div>');
+	$('.container').append('<div class="buttons"></div>');
 	$('.numClicks').replaceWith('<p class="numClicks">Click Number: ' + counter + '</p>');
 
 
-	var $el = $('#generate').children().last();
+	var $el = $('.container').children().last();
 
 	$el.append('<button class="delete">Delete</button>');
 	$el.append('<button class="change">Change</button>');
 }
 
+function deleteButton() {
+	$(this).parent().remove();
+}
+
+function changeColor() {
+	$(this).parent().addClass('highlight');
+}
